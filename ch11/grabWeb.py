f46e8f4760a806d4dad70308132abb42520bd5ef
metideas <metideas@outlook.com>
2016-06-13 00:14:30 -0400

    grabWeb
    Chapter 11, passing arguments to functions

---
+++ ch11/grabWeb.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# !/usr/bin/env python
+
+from urllib import urlretrieve
+
+
+def firstNonBlank(lines):
+    for eachLine in lines:
+        if not eachLine.strip():
+            continue
+        else:
+            return eachLine
+
+
+def firstLast(webpage):
+    f = open(webpage)
+    lines = f.readlines()
+    f.close()
+    print firstNonBlank(lines), lines.reverse()
+    print firstNonBlank(lines)
+
+
+def download(url='http://www.w3c.com', process=firstLast):
+    try:
+        retval = urlretrieve(url)[0]
+    except IOError:
+        retval = None
+    if retval:  # do some processing
+        process(retval)
+
+
+if __name__ == '__main__':
+    download()
